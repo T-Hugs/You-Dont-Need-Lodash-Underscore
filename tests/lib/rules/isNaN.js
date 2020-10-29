0c1adc14da509457830b58a43e35c8493758cd2f
Trevor Gau <trevorsg@gmail.com>
2020-10-29 11:09:49 -0400

    Add auto fixer for _.isNaN

---
+++ tests/lib/rules/isNaN.js
@@ -0,0 +1,25 @@
+"use strict";
+const RuleTester = require("eslint").RuleTester;
+const assert = require("assert");
+const rules = require("../../../lib/rules/all");
+const allRules = require("../../../lib/rules/rules");
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 2018, sourceType: "module" },
+});
+
+ruleTester.run("_.isNaN", rules.isNaN, {
+  invalid: [
+    {
+      code: `console.log(_.isNaN(42))`,
+      errors: ["Consider using the native Number.isNaN()"],
+      output: `console.log(Number.isNaN(42))`,
+    },
+    {
+      code: `console.log(_.isNaN("asdf"))`,
+      errors: ["Consider using the native Number.isNaN()"],
+      output: `console.log(Number.isNaN("asdf"))`,
+    },
+  ],
+  valid: [],
+});
